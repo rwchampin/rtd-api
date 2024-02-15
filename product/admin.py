b3cbab072d6be4bcf90fdc58edfe3d65b8b27186
Ryan Champin <rwchampin@gmail.com>
2024-02-15 11:35:03 -0500

    k

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Product, ProductAttribute, ProductTag, ProductType
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ProductAttribute)
+admin.site.register(ProductTag)
+admin.site.register(ProductType)
